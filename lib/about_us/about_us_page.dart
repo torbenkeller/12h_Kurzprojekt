5093bf7e63cb3290e88f0f5a63aec898a75b21ce
Torben Keller <torben.keller@tennisrockt.com>
2019-10-14 14:31:26 +0200

    fixes #13

---
+++ lib/about_us/about_us_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AboutUsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+//    return Padding(
+//      padding: const EdgeInsets.all(8.0),
+//      child: SingleChildScrollView(child: Column(children: <Widget>[
+//        Text('Über Uns', style: Theme.of(context).textTheme.display1,)
+//      ],),),
+//    );
+    return Center(
+      child: Text('Über uns'),
+    );
+  }
+}
